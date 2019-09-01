fb256abbb6e11884effc809ee9d66e4562008d67
League Student <league@iMac-12.attlocal.net>
2019-09-01 14:32:10 -0700

    harry potter

--- src/_03_harry_potter/Harrypotteryeye.java
+++ src/_03_harry_potter/Harrypotteryeye.java
@@ -1,5 +1,15 @@
+
 package _03_harry_potter;
 
 public class Harrypotteryeye {
+public static void main(String[] args) {
+	
 
+	HarryPotter harrypotter = new HarryPotter();
+harrypotter.makeInvisible(true);
+harrypotter.spyOnSnape();
+harrypotter.makevisible(true);
+
+
+}
 }
