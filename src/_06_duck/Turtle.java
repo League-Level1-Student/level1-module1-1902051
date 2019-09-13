090bd904917927f5aa605cf00ff47bcef4eb743b
League Student <league@iMac-20.attlocal.net>
2019-09-12 19:57:52 -0700

    duck and turtle (Got so much practice on contrustors)
    :neutral_face:

---
+++ src/_06_duck/Turtle.java
@@ -0,0 +1,25 @@
+package _06_duck;
+
+public class Turtle {
+String color;
+boolean canswim;
+void swim  () {
+	
+}
+	void layegg () {
+		
+	}
+	Turtle (String color, boolean canswim) {
+		this.color = color;
+		this.canswim = canswim;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
